f2309bfcc51da251f865fb222d08dc03151efda7
Otavio <luiz_otavio71@hotmail.com>
2021-03-07 07:57:58 -0300

    Gerando CRUD Scarffolding a partir do Controlador e foi trocadoo tema base usando bootstrap4

---
+++ ProductStock/Data/ProductStockContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductStock.Models.Produto;
+
+namespace ProductStock.Models
+{
+    public class ProductStockContext : DbContext
+    {
+        public ProductStockContext (DbContextOptions<ProductStockContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductStock.Models.Produto.Produto> Produto { get; set; }
+    }
+}
---
+++ ProductStock/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using ProductStock.Models.ViewModel;
+
+namespace ProductStock.Models.ViewModel
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
